73d292d65b0a585956b3cc849912b80594ff51da
dlmade <dlmadeblog@gmail.com>
2023-09-09 15:45:10 +0530

    check updates

--- code/app.py
+++ code/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World from Flask</h2>'
+    return '<h1>Hello World from Flask updates</h2>'
 
 if __name__ == "__main__":
     app.run(debug=True)
